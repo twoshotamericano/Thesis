60a57037d13a98c67afd9f99a1d65a9f559a5511
twoshotamericano <edanderson987@gmail.com>
2017-04-02 14:05:53 +0200

    Data files

---
+++ Analysis/Data/Gather1.R
@@ -0,0 +1,13 @@
+################
+# R source file created to download the data for Housing Analysis
+# Created by Ed Anderson
+# 19/03/2017
+################
+
+# Download the package to access external data
+# Not possible as the data is not hosted online in an accessible format
+# Input file saved as kc_house_data.csv
+# File obtained from Kaggle under Kings County Property Prices
+# Output file saved as data2
+
+
